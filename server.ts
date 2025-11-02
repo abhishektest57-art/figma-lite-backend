0f2824d668b395a5c3653bfd3bb6dfc26744e9d9
Abhishek Kumar <abhi.vickycool@gmail.com>
2025-11-02 20:32:00 +0530

    ..

--- server.ts
+++ server.ts
@@ -36,7 +36,8 @@ app.use(cors({
 }));
 
 // ✅ This must come *before* your routes
-app.options('/*', cors());
+// app.options('/*', cors());
+app.options(/.*/, cors());
 
 // Mounting REST routes
 app.use('/api/designs', designRoutes);
